de61828c34ca13fe49bd3244fbd7dc7ed86a9cf1
MLSkadi <108262760+2448309821@users.noreply.github.com>
2026-04-21 13:43:48 +0900

    fix missing React import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { buildSchedule, sessions, statusOptions, teacherOrder } from './schedule'
 
 const STORAGE_KEY = 'rotation-web-state-v1'
